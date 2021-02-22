1b7e23134036fa0aec5d5f71f53d046b9488b5e9
Sean WIlson <sean.wilson@undergroundcreative.com>
2021-02-22 12:28:34 +0000

    Getting back to re-revert situation
    Had reverted to a previous commit as plugin wouldn't install due to errors. Later realised it was because I'd changed the visibility to private. Putting latest changes back.

--- www/QuickSDKPlugin.js
+++ www/QuickSDKPlugin.js
@@ -1,71 +1,65 @@
-cordova.define("com-undergroundcreative-quicksdk.QuickSDKPlugin", function(require, exports, module) {
+var argscheck = require('cordova/argscheck');
+var channel = require('cordova/channel');
+var exec = require('cordova/exec');
+var cordova = require('cordova');
 
-        var argscheck = require('cordova/argscheck');
-        var channel = require('cordova/channel');
-        var exec = require('cordova/exec');
-        var cordova = require('cordova');
+//channel.createSticky('onCordovaInfoReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+//channel.waitForInitialization('onCordovaInfoReady');
 
-        //channel.createSticky('onCordovaInfoReady');
-        // Tell cordova channel to wait on the CordovaInfoReady event
-        //channel.waitForInitialization('onCordovaInfoReady');
+function QuickSDKPlugin () {
+    this.available = false;
+    this.platform = null;
+    this.version = null;
+    this.uuid = null;
+    this.cordova = null;
+    this.model = null;
+    this.manufacturer = null;
+    this.isVirtual = null;
+    this.serial = null;
 
-        function QuickSDKPlugin () {
-            this.available = false;
-            this.platform = null;
-            this.version = null;
-            this.uuid = null;
-            this.cordova = null;
-            this.model = null;
-            this.manufacturer = null;
-            this.isVirtual = null;
-            this.serial = null;
+    var me = this;
 
-            var me = this;
+    channel.onCordovaReady.subscribe(function () {
+        console.log("JS QuickSDKPlugin 1");
+    });
+}
 
-            channel.onCordovaReady.subscribe(function () {
-                console.log("JS QuickSDKPlugin 1");
-            });
-        }
+QuickSDKPlugin.prototype.helloWorld = function (arg0, successCallback, errorCallback) {
+    //argscheck.checkArgs('fF', 'QuickSDKPlugin.getInfo', arguments);
+    console.log("JS helloWorld 1");
+    exec(successCallback, errorCallback, 'QuickSDKPlugin', 'echo', [arg0]);
+    console.log("JS helloWorld 2");
+};
 
-        QuickSDKPlugin.prototype.helloWorld = function (arg0, successCallback, errorCallback) {
-            //argscheck.checkArgs('fF', 'QuickSDKPlugin.getInfo', arguments);
-            console.log("JS helloWorld 1");
-            exec(successCallback, errorCallback, 'QuickSDKPlugin', 'echo', [arg0]);
-            console.log("JS helloWorld 2");
-        };
+QuickSDKPlugin.prototype.initialise = function (successCallback, errorCallback) {
+    console.log("JS QuickSDKPlugin initialise 1");
+    exec(successCallback, errorCallback, 'QuickSDKPlugin', 'initialise');
+    console.log("JS QuickSDKPlugin initialise 2");
+};
 
-        QuickSDKPlugin.prototype.initialise = function (successCallback, errorCallback) {
-            console.log("JS QuickSDKPlugin initialise 1");
-            exec(successCallback, errorCallback, 'QuickSDKPlugin', 'initialise');
-            console.log("JS QuickSDKPlugin initialise 2");
-        };
+QuickSDKPlugin.prototype.login = function (successCallback, errorCallback) {
+    console.log("JS QuickSDKPlugin login 1");
+    exec(successCallback, errorCallback, 'QuickSDKPlugin', 'login');
+    console.log("JS QuickSDKPlugin login 2");
+};
 
-        QuickSDKPlugin.prototype.login = function (successCallback, errorCallback) {
-            console.log("JS QuickSDKPlugin login 1");
-            exec(successCallback, errorCallback, 'QuickSDKPlugin', 'login');
-            console.log("JS QuickSDKPlugin login 2");
-        };
+QuickSDKPlugin.prototype.logout = function (successCallback, errorCallback) {
+    console.log("JS QuickSDKPlugin logout 1");
+    exec(successCallback, errorCallback, 'QuickSDKPlugin', 'logout');
+    console.log("JS QuickSDKPlugin logout 2");
+};
 
-        QuickSDKPlugin.prototype.logout = function (successCallback, errorCallback) {
-            console.log("JS QuickSDKPlugin logout 1");
-            exec(successCallback, errorCallback, 'QuickSDKPlugin', 'logout');
-            console.log("JS QuickSDKPlugin logout 2");
-        };
+QuickSDKPlugin.prototype.pay = function (successCallback, errorCallback, name, id, price) {
+    console.log("JS QuickSDKPlugin pay 1");
+    exec(successCallback, errorCallback, 'QuickSDKPlugin', 'pay', [name, id, price]);
+    console.log("JS QuickSDKPlugin pay 2");
+};
 
-        QuickSDKPlugin.prototype.pay = function (successCallback, errorCallback, name, id, price) {
-            console.log("JS QuickSDKPlugin pay 1");
-            exec(successCallback, errorCallback, 'QuickSDKPlugin', 'pay', [name, id, price]);
-            console.log("JS QuickSDKPlugin pay 2");
-        };
+QuickSDKPlugin.prototype.testMethod = function (successCallback, errorCallback) {
+    console.log("JS QuickSDKPlugin testMethod 1");
+    exec(successCallback, errorCallback, 'QuickSDKPlugin', 'testMethod');
+    console.log("JS QuickSDKPlugin testMethod 2");
+};
 
-        QuickSDKPlugin.prototype.testMethod = function (successCallback, errorCallback) {
-            console.log("JS QuickSDKPlugin testMethod 1");
-            exec(successCallback, errorCallback, 'QuickSDKPlugin', 'testMethod');
-            console.log("JS QuickSDKPlugin testMethod 2");
-        };
-
-        module.exports = new QuickSDKPlugin();
-
-
-
-});
+module.exports = new QuickSDKPlugin();
